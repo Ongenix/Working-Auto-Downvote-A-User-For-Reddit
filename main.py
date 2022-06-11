ae1554763e037990aabcfe536a3824231cda6755
Ongenix <96583029+Ongenix@users.noreply.github.com>
2022-06-11 09:29:01 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import os;os.system('pip3 install praw') #setup
+import praw
+#===================
+# You can get the data for this at ssl.reddit.com/prefs/apps and create a new script
+# The username and password part is your username and password
+r = praw.Reddit(user_agent='Auto-Downvote',
+                client_id='',
+                client_secret='',
+                username="",
+                password='')
+#===================
+
+limit = 1000 #keep this the same
+user = ''
+#person to stalk
+downvote = True
+upvote = False
+debug = False
+
+for i in ((r.redditor(user).submissions.new(limit=limit))):
+  comment = r.submission(i.id)
+  try:
+    if downvote and not upvote:
+      comment.downvote()
+    else:
+      comment.upvote()
+  except Exception as e:
+    if debug:
+      print(e)
+     else:1
